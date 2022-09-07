e84ca09778e15c3b403d071fdf4326a8122cfdfd
Guzel <muhametova-guzel@mail.ru>
2022-09-07 18:39:39 +0300

    template init

---
+++ src/db/models/checklist.js
@@ -0,0 +1,13 @@
+class Checklist {
+  /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+  // static associate(models) {
+  //   // define association here
+  // }
+  // constructor(q1,q2,q3,q4,q5,q6,q7,q8,q9,q10,q11,q12)
+}
+
+module.exports = Checklist;
