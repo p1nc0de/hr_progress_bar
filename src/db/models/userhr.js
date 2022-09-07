b8524acb6471b2cec160ba48abe5e29af2f184ee
p-force <pforce.07.2021@gmail.com>
2022-09-07 18:32:58 +0300

    darabase-migrations

---
+++ src/db/models/userhr.js
@@ -0,0 +1,26 @@
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class UserHr extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  UserHr.init({
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    passwd: DataTypes.TEXT,
+    admin: DataTypes.BOOLEAN,
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE,
+  }, {
+    sequelize,
+    modelName: 'UserHr',
+  });
+  return UserHr;
+};
