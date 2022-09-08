b6dce4a1a68ba410296d90c1d34cc0e501b0a7b6
Guzel <muhametova-guzel@mail.ru>
2022-09-08 21:52:37 +0300

    newEmp db

--- src/components/AddNewEmployee.jsx
+++ src/components/AddNewEmployee.jsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function AddNewEmployee() {
+  const [inputs, setInputs] = useState({ userName: '', menthorName: '' });
+  const inputHandler = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
   const submitHandler = () => {
-
+    fetch('/employees/new', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(inputs),
+    });
   };
   return (
     <>
@@ -11,17 +21,19 @@ export default function AddNewEmployee() {
         <div className="mb-3">
           <label htmlFor="exampleInputEmail1" className="form-label">
             Имя нового сотрудника
-            <input type="text" className="form-control" id="exampleInputEmail1" />
+            <input type="text" name="userName" value={inputs.userName} onChange={inputHandler} className="form-control" id="exampleInputEmail1" />
           </label>
         </div>
         <div className="mb-3">
           <label htmlFor="exampleInputPassword1" className="form-label">
             Имя ментора нового сотрудника
-            <input type="text" className="form-control" id="exampleInputPassword1" />
+            <input type="text" name="menthorName" value={inputs.menthorName} onChange={inputHandler} className="form-control" id="exampleInputPassword1" />
           </label>
         </div>
-        <button type="submit" className="btn btn-primary">Создать</button>
+        <a type="submit" className="btn btn-primary" href="">Создать</a>
       </form>
     </>
   );
 }
+
+// `/${newEmp.uniqueUrl}`
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,20 +6,23 @@ import Navbar from './navbar/Navbar';
 import Templates from './Templates';
 import MyTemplates from './MyTemplates';
 import AddNewEmployee from './AddNewEmployee';
+import NewEmployee from './NewEmployee';
 
-function App({ lists, myLists, list, userEmail }) {
+function App({
+  lists, myLists, list, userEmail, newEmp,
+}) {
   const [checklists, setChecklists] = useState(lists);
   const [myChecklists, setMyChecklists] = useState(myLists);
   const [user, setUser] = useState(userEmail || null);
   return (
     <>
       <Navbar setChecklists={setChecklists} setMyChecklists={setMyChecklists} />
-      <Home />
       <Routes>
         <Route path="/" element={<Home checklists={checklists} setUser={setUser} />} />
         <Route path="/templates" element={<Templates checklists={checklists} setChecklists={setChecklists} />} />
         <Route path="/templates/:id" element={<MyTemplates myChecklists={myChecklists} setMyChecklists={setMyChecklists} />} />
-        <Route path="/add" element={<AddNewEmployee setChecklists={setChecklists} setMyChecklists={setMyChecklists} />} />
+        <Route path="employees/new" element={<AddNewEmployee setChecklists={setChecklists} setMyChecklists={setMyChecklists} />} />
+        <Route path="employees/:id" element={<NewEmployee newEmp={newEmp} />} />
         <Route path="/checklists/:id" element={<Checklist list={list} />} />
       </Routes>
     </>
