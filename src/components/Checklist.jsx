48d4a230a22953a4e5d41512680d78cd8ee431e2
p-force <pforce.07.2021@gmail.com>
2022-09-08 15:04:34 +0300

    checklist

---
+++ src/components/Checklist.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+//
+function Checklist({user}) {
+  return (
+    <>
+      <div className="form-check">
+        <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+        <label className="form-check-label" htmlFor="flexCheckDefault">
+          Default checkbox
+        </label>
+      </div>
+      <div className="form-check">
+        <input className="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked />
+        <label className="form-check-label" htmlFor="flexCheckChecked">
+          Checked checkbox
+        </label>
+      </div>
+    </>
+  );
+}
+
+export default Checklist;
