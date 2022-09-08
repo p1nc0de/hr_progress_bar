20ccb91c45d93c438a53f199472a951d3420d74c
p-force <pforce.07.2021@gmail.com>
2022-09-08 17:01:30 +0300

    component Checklist

--- src/components/Checklist.jsx
+++ src/components/Checklist.jsx
@@ -1,22 +1,43 @@
 import React from 'react';
 
 //
-function Checklist({checklist}) {
+function Checklist({ list }) {
   return (
-    <>
-      <div className="form-check">
-        <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
-        <label className="form-check-label" htmlFor="flexCheckDefault">
-          Default checkbox
-        </label>
+    <div className="mx-auto" style={{ width: '940px' }}>
+      <p className="fs-5 text">
+        <p className="lh-1">
+          <p className="text-start">
+            Привет,
+            {' '}
+            {list.userName}
+          </p>
+          <p className="text-start">
+            И добро пожаловать в команду Высокогорья!
+          </p>
+          <p className="text-start">
+            Впереди нас ждет интересное путешествие в мир нашей компании,
+            и самым главным проводником будет -
+            {' '}
+            {list?.menthorName}
+          </p>
+        </p>
+      </p>
+      <p className="fs-6">
+        Мы подготовили для тебя чек-лист на первый день.
+        Процесс выполнения будет сохраняться, поэтому ты можешь
+        закрывать пункты в удобном для тебя порядке.
+      </p>
+      <div>
+        Нужно подготовить твое рабочее место:
+        <div className="form-check">
+
+          <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+          <label className="form-check-label" htmlFor="flexCheckDefault">
+            Наставник выдал мне пропуск
+          </label>
+        </div>
       </div>
-      <div className="form-check">
-        <input className="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked />
-        <label className="form-check-label" htmlFor="flexCheckChecked">
-          Checked checkbox
-        </label>
-      </div>
-    </>
+    </div>
   );
 }
 
