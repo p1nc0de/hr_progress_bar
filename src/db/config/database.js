f272c9a4faff39cce77a5ed63ba89154a2604e98
Dmitriy <dmitriy.p.pinchuk@gmail.com>
2022-09-09 15:44:55 +0300

    deploy

--- src/db/config/database.js
+++ src/db/config/database.js
@@ -22,5 +22,8 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: 'postgres',
+    dialectOptions: {
+      ssl: { rejectUnauthorized: false },
+    },
   },
 };
